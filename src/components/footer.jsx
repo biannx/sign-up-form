07db1178c53aa2a09221c78b3d5fd8f9f175fedb
Bianca Aquino <lyahbianca.aquino04@gmail.com>
2023-02-14 21:21:12 +0800

    Setup vite, styled, finished form.

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <footer className='mt-5 pt-5 d-flex justify-content-center align-items-center text-light'>
+            <span>
+            <i className="fa-solid fa-copy text-dark px-1"></i>
+                Template from Scrimba
+            </span>
+            <span className="px-2">
+            <i className="fa-solid fa-copyright text-dark px-1"></i>
+                Developed by: Bianca Aquino</span>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header className='my-3 d-flex justify-content-center align-items-center text-light'>
+          <h3 className="fw-bold">Sign Up</h3>
+        </header>
+    )
+}
+
+export default Header
